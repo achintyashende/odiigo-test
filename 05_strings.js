9d4abb10d5742c74f9a0753673b057862eb4f504
jadhavswaraj <jswaraj48@gmail.com>
2025-03-03 15:13:11 +0530

    changes in strings file

--- 05_strings.js
+++ 05_strings.js
@@ -1,12 +1,12 @@
-const name = "Achintya"
-const repoCount = 50
+const name = "Achintya";
+const repoCount = 50;
 
 // console.log(name + repoCount + " values");
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
 // string decelration
-const myName = "Achintya"
-const gameName = new String("Among-Us")
+const myName = "Achintya";
+const gameName = new String("Among-Us");
 
 console.log(myName);
 console.log(myName[0]);
@@ -16,20 +16,21 @@ console.log(gameName[2]);
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
-console.log(gameName.indexOf('g'));
+console.log(gameName.indexOf("g"));
 
-const newString = gameName.substring(0,4)
+const newString = gameName.substring(0, 4);
 console.log(newString);
 
-const newStr = "   achintya she dededjen  "
+const newStr = "   achintya she dededjen  ";
 console.log(newStr);
 console.log(newStr.trim());
 
+const url = "https://achintyashende%20versal.app";
 
-const url = "https://achintyashende%20versal.app"
+console.log(url.replace("%20", "-"));
+console.log(url.includes("achintya"));
 
-console.log(url.replace('%20', '-'));
-console.log(url.includes('achintya'));
+console.log(gameName.split("-"));
 
-
-console.log(gameName.split('-'));
+console.log("Swaraj-Achintya");
+// this is the change
