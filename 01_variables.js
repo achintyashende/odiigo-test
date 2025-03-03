0b703f4838ffc70c865b8a90b953ef1fa46cd00c
jadhavswaraj <jswaraj48@gmail.com>
2025-03-03 13:31:52 +0530

    Your commit message

--- 01_variables.js
+++ 01_variables.js
@@ -1,16 +1,18 @@
-const accountId = 9999
-let accountEmail = "swarajjadhav@gmail.com"
-var accountPassword = "123#"
-accountCity = "Nagpur"
-let accountState
+//hii
+
+const accountId = 9999;
+let accountEmail = "swarajjadhav@gmail.com";
+var accountPassword = "123#";
+accountCity = "Nagpur";
+let accountState;
 
 // accountId = 2  // not allowed
 console.log(accountId);
 
-accountEmail = "achintyashende108@gmail.com"
-accountPassword = "coding#123"
-accountCity = "Wardha"
+accountEmail = "achintyashende108@gmail.com";
+accountPassword = "coding#123";
+accountCity = "Wardha";
 
-console.table([accountEmail,accountPassword,accountCity, accountState])
+console.table([accountEmail, accountPassword, accountCity, accountState]);
 
-console.log(typeof(12 + "Achintya" + 9999));
+console.log(typeof (12 + "Achintya" + 9999));
